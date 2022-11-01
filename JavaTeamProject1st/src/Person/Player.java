13df0f890a2641dd8b22bc51084894212bf4fa8c
921126 <nabom921126@gmail.com>
2022-11-01 14:15:39 +0900

    2022.10.01 14:15 플레이어 움직임 추가

--- JavaTeamProject1st/src/Person/Player.java
+++ JavaTeamProject1st/src/Person/Player.java
@@ -1,12 +1,14 @@
 package Person;
 
+import java.util.Scanner;
+
 public class Player extends Common{
-	
+
 	private String name = null;
 	private int money = 0;
 	private int x = 0;
 	private int y = 0;
-	
+
 	//생성자
 	public Player() {
 		setHp(10);
@@ -52,5 +54,52 @@ public class Player extends Common{
 		System.out.println(" 이름:" + getName() + " ◆ " + "체력:" + getHp() + " ◆ " + "공격력:" + getAttack() + " ◆ " + "돈:" + getMoney() + "골드" );
 		System.out.println(" = = = = = = = = = = = = = = = = = = = = =");
 	}
-	
+
+	public void playerMove() {
+		int moveX = 0;
+		int moveY = 0;
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+
+		switch (str) {
+		case "w": case "W":
+			
+			if(getX() == 0) {
+				System.out.println("이동이 불가능합니다.");
+			}else {
+				moveX = getX()-1;
+				setX(moveX);
+			}
+
+			break;
+		case "a": case "A":
+			if(getY() == 0) {
+				System.out.println("이동이 불가능합니다.");
+			}else {
+				moveY = getY()-1;
+				setY(moveY);
+			}
+			break;
+		case "s": case "S":
+			if(getX() == 6) {
+				System.out.println("이동이 불가능합니다.");
+			}else {
+				moveX = getX()+1;
+				setX(moveX);
+			}
+			break;
+		case "d": case "D":
+			if(getY() == 6) {
+				System.out.println("이동이 불가능합니다.");
+			}else {
+				moveY = getY()+1;
+				setY(moveY);
+			}
+			break;
+		default:
+			System.out.println(" w a s d 키로 입력해주세요.");
+			break;
+		}
+	}
+
 }
